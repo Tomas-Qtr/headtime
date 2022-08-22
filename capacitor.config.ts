32e3bae5f64174be016fe5c14c110368ca919917
Tomas-Qtr <tomi30.quinteros@gmail.com>
2022-08-22 14:28:16 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'headtime',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
